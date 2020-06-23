54919a448b25ac1d60b6958de44d02632a141b07
wilantury <ventas@antury.net>
2020-06-23 16:54:31 -0500

    [Update] module Calc

--- calculadora.go
+++ calculadora.go
@@ -1,1 +1,49 @@
 package mycalculator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type calc struct {
+}
+
+func getOperator(in string) string {
+	var validID = regexp.MustCompile(`[\+*-/]`)
+	fmt.Println(validID.FindString(in))
+	return validID.FindString(in)
+}
+
+func (calc) operate(entrada string, operator string) int {
+	clean_input := strings.Split(entrada, operator)
+	operator1 := parsear(clean_input[0])
+	operator2 := parsear(clean_input[1])
+	switch operator {
+	case "-":
+		return operator1 - operator2
+	case "+":
+		return operator1 + operator2
+	case "*":
+		return operator1 * operator2
+	case "/":
+		return operator1 / operator2
+	default:
+		fmt.Println("operador no soportado")
+		return 0
+	}
+}
+
+func LeerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text_ := scanner.Text()
+	return text_
+}
+func parsear(entrada string) int {
+	input, _ := strconv.Atoi(entrada)
+	return input
+}
